f77c82ae792a6ae5bdcec5662d4caf59efdd2c25
Alessandra Somma <alessandra.somma@unina.it>
2024-08-30 18:22:19 +0200

    update broker and shadow manager, not yet complete and runnable

--- libraries/classes/Broker.py
+++ libraries/classes/Broker.py
@@ -1,12 +1,14 @@
 from ngsildclient import Client
+from typing import Optional, List
+
 
 class Broker:
     portNumber: int
-    portTemporal: int
+    portTemporal: Optional[int]
     fiwareService: str
     hostname: str
 
-    def __init__(self, pn: int, pnt: int, host: str, fiwareservice:str):
+    def __init__(self, pn: int, pnt: Optional[int], host: str, fiwareservice: str):
         self.portNumber = pn
         self.portTemporal = pnt
         self.hostname = host
@@ -22,6 +24,15 @@ class Broker:
             return cb
         except Exception as e:
             raise ConnectionError(f"Impossible to connect: {str(e)}")
+
+
+    def updateContext(self, deviceid, timeSlot: str, trafficFlow: int, coordinates: List, laneDirection: str):
+        # riceve il dato -> chiede allo shadow manager la shadow
+        # ottiene la shadow che descrive strada
+        # dalla strada posso cercare l'entity
+        return True
+
+
 
 
 
--- libraries/classes/DigitalShadowManager.py
+++ libraries/classes/DigitalShadowManager.py
@@ -1,3 +1,47 @@
+from typing import Dict, List
+from libraries.constants import tlPath
+import pandas as pd
+
+
+class Shadow:
+    name: str
+
+    def __init__(self, name: str, sdata):
+        self.name = name
+        self.data = sdata
+
+
+
+class DataProcessor:
+
+    def __init__(self, datapath):
+        self.df = pd.read_csv(datapath)
+
+    def processData(self, timeSlot: str, trafficFlow: int, coordinates: List[float], direction: str) -> str:
+        row = self.df.iloc
+        roadName = self.df
+        return roadName
+
+
 
 class DigitalShadowManager:
-    shadowType: str
+    def __init__(self):
+        self.shadowsByTypes: Dict[str, List[Shadow]] = {}
+        self.dataProcessor = DataProcessor(tlPath)
+
+
+    def addshadow(self, shadowtype: str, timeSlot: str, trafficFlow: int, coordinates: List[float], direction: str):
+        roadName=self.dataProcessor.processData()
+        newShadow = Shadow(name=roadName)
+        newShadow # add attributes timeSlot: str, trafficFlow: int, coordinates: List, direction: str
+        if shadowType not in self.shadowsByTypes:
+            self.shadowsByTypes[shadowtype] = []
+
+        self.shadowsByTypes[shadowtype].append()
+
+
+        return True
+
+
+
+
