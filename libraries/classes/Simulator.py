588916e28cc0ef2b2c213d1d7f46bb40145b2b14
manfr <manfredinapolitano@gmail.com>
2024-09-03 12:35:29 +0200

    Changed name of simulator_adapter to Simulator. Added some basic function for managing tls. First use of subscription mechanism available in libtraci.

--- libraries/classes/Simulator.py
+++ libraries/classes/Simulator.py
@@ -23,6 +23,7 @@ class Simulator:
     configurationPath: str
     logFile: str
     listener: libtraci.StepListener
+    vehicleSummary: {}
 
     def __init__(self,  configurationpath: str, logfile: str):
         self.configurationPath = configurationpath
@@ -30,15 +31,12 @@ class Simulator:
         self.listener = ValueListener()
         libtraci.addStepListener(self.listener)
 
-
-
     def start(self):
         libtraci.start(["sumo", "-c", self.configurationPath + "run.sumocfg"], traceFile=self.logFile)
         print("Note that a simulation step is equivalent to " + str(libtraci.simulation.getDeltaT()) + " seconds")
+        self.subscribeToInductionLoop("0.127_2.73_6_1__l0", "vehicleNumber")
         # self.oneHourStep()
         self.resume()
-
-
 
     def step(self, quantity=1):
         '''
@@ -48,8 +46,11 @@ class Simulator:
         step = 0
         while step < quantity and self.getRemainingVehicles() > 0:
             libtraci.simulationStep()
-            # vehiclesSummary = self.getVehiclesSummary()
-            inductionLoopSummary = self.getInductionLoopSummary()
+            vehiclesSummary = self.getVehiclesSummary()
+            # inductionLoopSummary = self.getInductionLoopSummary()
+            self.findLinkedTLS("0.127_2.73_6_1__l0")
+            value = libtraci.inductionloop.getAllSubscriptionResults()
+            # self.setTLSProgram("219", "utopia")
             print(self.getRemainingVehicles())
             step += 1
 
@@ -66,13 +67,13 @@ class Simulator:
     def end(self):
         return libtraci.close()
 
+
     def getRemainingVehicles(self):
         # this function returns the vehicles inside the simulation plus the ones that are waiting to start
         return libtraci.simulation.getMinExpectedNumber()
 
 
 ### VEHICLE FUNCTIONS
-
     def getVehiclesSummary(self):
         vehicleSummary = {}
         vehiclesList = libtraci.vehicle.getIDList()
@@ -84,33 +85,33 @@ class Simulator:
                 element["timeLost"] = libtraci.vehicle.getTimeLoss(vehicleID)
                 element["distance"] = libtraci.vehicle.getDistance(vehicleID)
                 element["departDelay"] = libtraci.vehicle.getDepartDelay(vehicleID)
+                element["totalWaitingTime"] = libtraci.vehicle.getAccumulatedWaitingTime(vehicleID)
                 summary.append(element)
 
             vehicleSummary["averageSpeed"] = mean(element["speed"] for element in summary)
             vehicleSummary["averageTimeLost"] = mean(element["timeLost"] for element in summary)
             vehicleSummary["averageDepartDelay"] = mean(element["departDelay"] for element in summary)
+            vehicleSummary["averageWaitingTime"] = mean(element["totalWaitingTime"] for element in summary)
             print("The Average Speed of Vehicles is: " + str(vehicleSummary["averageSpeed"]) + " m/s.")
             print("The Average Time lost is " + str(vehicleSummary["averageTimeLost"]) + " seconds.")
-            print("The Average depart delay is " + str(vehicleSummary["averageDepartDelay"]) + "seconds.")
+            print("The Average depart delay is " + str(vehicleSummary["averageDepartDelay"]) + " seconds.")
+            print("The Average time waited is " + str(vehicleSummary["averageWaitingTime"]) + " seconds.")
             return vehicleSummary
         print("There are no vehicles ")
         return None
 
-
+    def updateSummary(self):
+        # Not sure if it's useful include this data inside Simulator class
+        self.vehicleSummary = self.getVehiclesSummary()
 
 ### INDUCTION LOOP FUNCTIONS
-
     def getDetectorList(self):
         return libtraci.inductionloop.getIDList()
 
     def getAverageOccupationTime(self):
         detectorList = self.getDetectorList()
         intervalOccupancies = []
-
         for detector in detectorList:
-            # Although the function for obtaining IDs returns values correctly,
-            # once an id is entered for the search, it is not found
-            # TODO: find the problem
             intervalOccupancies.append(libtraci.inductionloop.getIntervalOccupancy(detector))
         average = mean(intervalOccupancies)
         print(average)
@@ -130,6 +131,52 @@ class Simulator:
         inductionLoopSummary["averageMeanSpeed"] = mean(element["meanSpeed"] for element in detectors)
         inductionLoopSummary["averageVehicleNumber"] = mean(element["vehicleNumber"] for element in detectors)
         return inductionLoopSummary
+
+
+    def findLinkedTLS(self, detectorID: str):
+        lane = libtraci.inductionloop.getLaneID(detectorID)
+        tls = self.getTLSList()
+        found = []
+        for element in tls:
+            lanes = libtraci.trafficlight.getControlledLanes(element)
+            if lane in lanes:
+                found.append(element)
+
+        return found
+
+
+    def subscribeToInductionLoop(self, inductionLoopID, value: str):
+        if value == "intervalOccupancy":
+            libtraci.inductionloop.subscribe(inductionLoopID, [libtraci.constants.VAR_INTERVAL_OCCUPANCY])
+        elif value == "meanSpeed":
+            libtraci.inductionloop.subscribe(inductionLoopID, [libtraci.constants.VAR_INTERVAL_SPEED])
+        elif value == "vehicleNumber":
+            libtraci.inductionloop.subscribe(inductionLoopID, [libtraci.constants.VAR_INTERVAL_NUMBER])
+
+
+
+### TLS FUNCTIONS
+    def getTLSList(self):
+        return libtraci.trafficlight_getIDList()
+
+    def checkTLS(self, tlsID):
+        tls = self.getTLSList()
+        if tlsID in tls:
+            return True
+        else:
+            return False
+
+
+    def setTLSProgram(self, trafficLightID : str, programID: str, all = False):
+    ### NOTE: There is still no check of existence of specific program inside the additional file
+        if all:
+            tls = self.getTLSList()
+            for traffic_light in tls:
+                libtraci.trafficlight.setProgram(traffic_light, programID)
+            print("The program of all traffic lights is changed to" + str(programID))
+        elif self.checkTLS(trafficLightID):
+            libtraci.trafficlight.setProgram(trafficLightID, programID)
+            print("The program of the TLS " + str(trafficLightID) + " is changed to " + str(programID))
 
 
 
