46064207eebd4b525581b180a8fcac07127cf3f0
Alessandra Somma <alessandra.somma@unina.it>
2024-09-16 09:23:25 +0200

    update constants.py

--- libraries/classes/Broker.py
+++ libraries/classes/Broker.py
@@ -1,6 +1,7 @@
-from ngsildclient import Client
+from ngsildclient import Client, Entity
 from typing import Optional, List
 from libraries.classes.DigitalShadowManager import DigitalShadowManager
+from libraries.constants import roadSegmentType
 
 class Broker:
     portNumber: int
@@ -33,12 +34,19 @@ class Broker:
 
         # search entity
         cbConnection=self.createConnection()
+        e = self.searchRoadSegmentEntity(cbConnection=cbConnection,edgeID=edgeID)
+        if e is not None:
+            return True #update della prop
+        else:
+            return True # creo l'entity --> devo avere a disposizione anche tutte le entity ad essa collegate, oppure fare funzioni per update
+        #delle singole prop
 
+    def searchRoadSegmentEntity(self, cbConnection: Client, edgeID: str) -> Entity:
+        generator = cbConnection.query_generator(type=roadSegmentType)
+        e: Entity | None = next((e for e in generator if e["edgeID"].value == edgeID), None)
+        return e
 
-        return True
-
-
-    def searchEntity(self, roadName: str) -> bool:
+    def updateFlow(self):
         return True
 
 
